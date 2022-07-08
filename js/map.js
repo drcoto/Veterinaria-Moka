bc22ba6ef2b250147fb7aab003f9d85ea78898fd
SubVray <jim.alberto.21@gmail.com>
2022-07-08 01:44:00 -0600

    version 0.0.7

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function iniciarMap() {
   let coord = { lat: 9.915635614486694, lng: -84.03693278154675 };
   let map = new google.maps.Map(document.getElementById("map"), {
-    zoom:10,
+    zoom:15,
     center: coord,
   });
   let marker = new google.maps.Marker({
@@ -40,20 +40,12 @@ function iniciarMap() {
   Costa Rica</p>
   <a href='https://www.google.com/maps'>Ver en google maps</a>`,
 });
-  addMarker({location:{ lat: 9.914172391872446, lng: -84.03946259864865 },ImageIcon:"",
-  content: 
-  `<h2>Veterinaria Moka</h2>
-  <p>San José, Curridabat<br>
-  11801<br>
-  Costa Rica</p>
-  <a href='https://www.google.com/maps'>Ver en google maps</a>`,
-});
-ddd
-  // informacion del waypoint
+//   addMarker({location:{ lat: 9.914172391872446, lng: -84.03946259864865 },ImageIcon:"",
+//   content: 
 //   `<h2>Veterinaria Moka</h2>
 //   <p>San José, Curridabat<br>
 //   11801<br>
 //   Costa Rica</p>
-//   <a href='https://www.google.com/maps'>Ver en google maps</a>
-// `,
+//   <a href='https://www.google.com/maps'>Ver en google maps</a>`,
+// });
 }
