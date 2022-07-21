70cb2ba61883fb70bf6e50fb5a9779d41e83f5e4
drcoto <106129878+drcoto@users.noreply.github.com>
2022-07-21 17:37:11 -0600

    puntos de control

---
+++ js/citas-listar.js
@@ -0,0 +1,61 @@
+const cuerpoTabla = document.querySelector('#tbl-users tbody');
+
+const llenarTabla = () => {
+    //Limpia el contenido que tiene el cuerpo de la tabla.
+    cuerpoTabla.innerHTML = '';
+    //Para cada usuario que se encuentre dentro de la coleccion de usuarios
+    citas.forEach(cita => {
+        let fila = cuerpoTabla.insertRow();
+
+        fila.insertCell().textContent = cita.fecha;
+        fila.insertCell().textContent = cita.nombreMascota;
+        fila.insertCell().textContent = cita.nombreOwner;
+        fila.insertCell().textContent = cita.procedimiento;
+        fila.insertCell().textContent = cita.doctor;
+
+        // Creación de la celda para los botones
+        let tdAcciones = fila.insertCell();
+
+        //Creación del boton editar
+        let btnEditar = document.createElement('button');
+        btnEditar.textContent = 'Editar';
+        btnEditar.type = 'button';
+        btnEditar.classList.add('btn-editar');
+        //Creacion del boton eliminar
+        let btnEliminar = document.createElement('button');
+        btnEliminar.textContent = 'Eliminar';
+        btnEliminar.type = 'button';
+        btnEliminar.classList.add('btn-eliminar');
+
+        //Agregar el boton de editar a la celda acciones
+        tdAcciones.appendChild(btnEditar);
+
+        //Agregar el boton de eliminar a la celda acciones
+        tdAcciones.appendChild(btnEliminar);
+
+        btnEliminar.addEventListener('click', () => {
+            Swal.fire({
+                title: 'Está seguro que desea eliminar la información?',
+                text: "La acción no se puede revertir!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, eliminar!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Eliminado!',
+                        'El usuario fue eliminado.',
+                        'success'
+                    )
+                }
+            })
+        });
+
+    });
+};
+
+
+
+llenarTabla();
---
+++ js/citas.js
@@ -0,0 +1,38 @@
+let citas = [{
+    'fecha': '05/05/2022',
+    'nombreMascota': 'Pelotita',
+    'nombreOwner': 'Alonso Alpizar',
+    'procedimiento': 'Vacuna',
+    'doctor': 'Dr. Luis Granera'
+
+}, {
+    'fecha': '03/03/2022',
+    'nombreMascota': 'Coco',
+    'nombreOwner': 'Alejandro Goicuria',
+    'procedimiento': 'Castracion',
+    'doctor': 'Dr. Luis Granera'
+}, {
+    'fecha': '20/03/2022',
+    'nombreMascota': 'Titan',
+    'nombreOwner': 'Gerardo Coto',
+    'procedimiento': 'Grooming',
+    'doctor': 'Dr. Luis Granera'
+}, {
+    'fecha': '10/01/2022',
+    'nombreMascota': 'Nieve',
+    'nombreOwner': 'Douglas Sequeira',
+    'procedimiento': 'Desparasitacion',
+    'doctor': 'Dr. Luis Granera'
+}, {
+    'fecha': '05/01/2022',
+    'nombreMascota': 'Samson',
+    'nombreOwner': 'Alexander Bermudez',
+    'procedimiento': 'Cita de control',
+    'doctor': 'Dr. Luis Granera'
+}, {
+    'fecha': '05/06/2022',
+    'nombreMascota': 'Guardian',
+    'nombreOwner': 'Gabriel Coto',
+    'procedimiento': 'Cirugia',
+    'doctor': 'Dr. Luis Granera'
+}];
