0c9ab79c39a3abc508e34376f7a4d35f2725815b
SubVray <jim.alberto.21@gmail.com>
2022-07-23 19:08:29 -0600

    prueba servicios 2

--- js/index.js
+++ js/index.js
@@ -8,53 +8,55 @@ const vacunacion = document.querySelector("#vacunacion");
 const emergencias = document.querySelector("#emergencias");
 
 let castracionesSelect = () => {
-document.getElementById("sct-castraciones").classList.add("card-select");
+  document.getElementById("sct-castraciones").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-castraciones").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 
 let cirugiasSelect = () => {
-document.getElementById("sct-cirugias").classList.add("card-select");
+  document.getElementById("sct-cirugias").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-cirugias").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 let desparacitacionesSelect = () => {
-document.getElementById("sct-desparasitaciones").classList.add("card-select");
+  document.getElementById("sct-desparasitaciones").classList.add("card-select");
   setTimeout(() => {
-    document.getElementById("sct-desparasitaciones").classList.remove("card-select");
-}, 3000);
+    document
+      .getElementById("sct-desparasitaciones")
+      .classList.remove("card-select");
+  }, 3000);
 };
 let groomingSelect = () => {
-document.getElementById("sct-grooming").classList.add("card-select");
+  document.getElementById("sct-grooming").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-grooming").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 let odontovetSelect = () => {
-document.getElementById("sct-odontovet").classList.add("card-select");
+  document.getElementById("sct-odontovet").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-odontovet").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 let radioSelect = () => {
-document.getElementById("sct-radio").classList.add("card-select");
+  document.getElementById("sct-radio").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-radio").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 let vacunacionSelect = () => {
-document.getElementById("sct-vacuna").classList.add("card-select");
+  document.getElementById("sct-vacuna").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-vacuna").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 let emergenciasSelect = () => {
-document.getElementById("sct-emergencias").classList.add("card-select");
+  document.getElementById("sct-emergencias").classList.add("card-select");
   setTimeout(() => {
     document.getElementById("sct-emergencias").classList.remove("card-select");
-}, 3000);
+  }, 3000);
 };
 
 castraciones.addEventListener("click", castracionesSelect);
@@ -65,3 +67,4 @@ odontovet.addEventListener("click", odontovetSelect);
 radio.addEventListener("click", radioSelect);
 vacunacion.addEventListener("click", vacunacionSelect);
 emergencias.addEventListener("click", emergenciasSelect);
+
