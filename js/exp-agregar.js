671a6e7d4bf8ff9dd03f33056f07fda9ab779630
drcoto <106129878+drcoto@users.noreply.github.com>
2022-07-24 19:01:55 -0600

    update

--- js/exp-agregar.js
+++ js/exp-agregar.js
@@ -2,12 +2,16 @@
 
 const botonLab = document.querySelector('#btn-lab');
 const botonTx = document.querySelector('#btn-agregarTx');
+const botonVac = document.querySelector('#btn-agregarVacuna');
 const dateLab = document.querySelector('#frm-labDate');
 const labPet = document.querySelector('#lab-pet');
 const labOwner = document.querySelector('#lab-owner');
 const labTest = document.querySelector('#lab-test');
 const labResult = document.querySelector('#lab-result');
-const botonVac = document.querySelector('#btn-agregarVacuna');
+const medDate = document.querySelector('#med-date');
+const medPet = document.querySelector('#med-pet');
+const medMed = document.querySelector('#med-med');
+const medDr = document.querySelectorAll('.frm-select');
 const vacunaDateInput = document.querySelector('#input-dateVac');
 const vacunaInput = document.querySelector('#input-vacuna');
 const txDate = document.querySelector('#input-txDate');
@@ -46,7 +50,7 @@ function llenarTablaVac() {
 
     btnEliminar.addEventListener('click', () => {
         Swal.fire({
-            title: 'Está seguro que desea eliminar la cita?',
+            title: 'Está seguro que desea eliminar la vacuna?',
             text: "La acción no se puede revertir!",
             icon: 'warning',
             showCancelButton: true,
@@ -68,7 +72,7 @@ function llenarTablaVac() {
             if (result.isConfirmed) {
                 Swal.fire(
                     'Eliminado!',
-                    'La cita fue eliminado.',
+                    'El registro fue eliminado.',
                     'success'
                 )
             }
@@ -107,7 +111,7 @@ function llenarTablaTx() {
 
     btnEliminar.addEventListener('click', () => {
         Swal.fire({
-            title: 'Está seguro que desea eliminar la cita?',
+            title: 'Está seguro que desea eliminar la evolución del procedimiento?',
             text: "La acción no se puede revertir!",
             icon: 'warning',
             showCancelButton: true,
@@ -129,95 +133,171 @@ function llenarTablaTx() {
             if (result.isConfirmed) {
                 Swal.fire(
                     'Eliminado!',
-                    'La cita fue eliminado.',
+                    'El registro fue eliminado.',
+                    'success'
+                )
+            }
+        })
+    })
+}
+
+function llenarTablaLab() {
+    let dFecha = dateLab.value;
+    let sPet = labPet.value;
+    let sOwner = labOwner.value;
+    let sTest = labTest.value;
+    let sResult = labResult.value;
+
+
+    let tbodyLab = document.querySelector("#tbl-lab tbody");
+    tbodyLab.innerHTML = "";
+    let fila = tbodyLab.insertRow();
+    let celdaDate = fila.insertCell();
+    let celdaPet = fila.insertCell();
+    let celdaOwner = fila.insertCell();
+    let celdaTest = fila.insertCell();
+    let celdaResult = fila.insertCell();
+    let tdAccionesLab = fila.insertCell();
+    celdaDate.innerHTML = dFecha;
+    celdaPet.innerHTML = sPet;
+    celdaOwner.innerHTML = sOwner;
+    celdaTest.innerHTML = sTest;
+    celdaResult.innerHTML = sResult;
+
+    let btnEditar = document.createElement('button');
+    btnEditar.textContent = 'Editar';
+    btnEditar.type = 'button';
+    btnEditar.classList.add('btn-editar');
+    //Creacion del boton eliminar
+    let btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.type = 'button';
+    btnEliminar.classList.add('btn-eliminar');
+
+    //Agregar el boton de editar a la celda acciones
+    tdAccionesLab.appendChild(btnEditar);
+
+    //Agregar el boton de eliminar a la celda acciones
+    tdAccionesLab.appendChild(btnEliminar);
+
+    btnEliminar.addEventListener('click', () => {
+        Swal.fire({
+            title: 'Está seguro que desea eliminar el exámen?',
+            text: "La acción no se puede revertir!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar!'
+        }).then((result) => {
+            tbodyLab.innerHTML = "";
+            let celdaDate = fila.insertCell();
+            let celdaPet = fila.insertCell();
+            let celdaOwner = fila.insertCell();
+            let celdaTest = fila.insertCell();
+            let celdaResult = fila.insertCell();
+            let tdAccionesLab = fila.insertCell();
+            celdaDate.innerHTML = " ";
+            celdaPet.innerHTML = " ";
+            celdaOwner.innerHTML = " ";
+            celdaTest.innerHTML = " ";
+            celdaResult.innerHTML = " ";
+            //Agregar el boton de editar a la celda acciones
+            tdAccionesLab.appendChild(btnEditar);
+
+            //Agregar el boton de eliminar a la celda acciones
+            tdAccionesLab.appendChild(btnEliminar);
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Eliminado!',
+                    'El registro fue eliminado.',
                     'success'
                 )
             }
         })
     });
 
-    function llenarTablaLab() {
-        let dFecha = dateLab.value;
-        let sPet = labPet.value;
-        let sOwner = labOwner.value;
-        let sTest = labTest.value;
-        let sResult = labResult.value;
+
+}
 
 
-        let tbodyLab = document.querySelector("#tbl-lab tbody");
-        tbodyLab.innerHTML = "";
-        let fila = tbodyLab.insertRow();
-        let celdaDate = fila.insertCell();
-        let celdaPet = fila.insertCell();
-        let celdaOwner = fila.insertCell();
-        let celdaTest = fila.insertCell();
-        let celdaResult = fila.insertCell();
-        let tdAccionesLab = fila.insertCell();
-        celdaDate.innerHTML = dFecha;
-        celdaPet.innerHTML = sPet;
-        celdaOwner.innerHTML = sOwner;
-        celdaTest.innerHTML = sTest;
-        celdaResult.innerHTML = sResult;
+function llenarTablaMed() {
+    let medFecha = medDate.value;
+    let medPet = medPet.value;
+    let medDr = medDr.value;
+    let medMed = medMed.value;
 
-        let btnEditar = document.createElement('button');
-        btnEditar.textContent = 'Editar';
-        btnEditar.type = 'button';
-        btnEditar.classList.add('btn-editar');
-        //Creacion del boton eliminar
-        let btnEliminar = document.createElement('button');
-        btnEliminar.textContent = 'Eliminar';
-        btnEliminar.type = 'button';
-        btnEliminar.classList.add('btn-eliminar');
+    console.log(medFecha, medPet, medDr, medMed);
 
-        //Agregar el boton de editar a la celda acciones
-        tdAccionesLab.appendChild(btnEditar);
-
-        //Agregar el boton de eliminar a la celda acciones
-        tdAccionesLab.appendChild(btnEliminar);
-
-        btnEliminar.addEventListener('click', () => {
-            Swal.fire({
-                title: 'Está seguro que desea eliminar la cita?',
-                text: "La acción no se puede revertir!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, eliminar!'
-            }).then((result) => {
-                tbodyLab.innerHTML = "";
-                let celdaDate = fila.insertCell();
-                let celdaPet = fila.insertCell();
-                let celdaOwner = fila.insertCell();
-                let celdaTest = fila.insertCell();
-                let celdaResult = fila.insertCell();
-                let tdAccionesLab = fila.insertCell();
-                celdaDate.innerHTML = " ";
-                celdaPet.innerHTML = " ";
-                celdaOwner.innerHTML = " ";
-                celdaTest.innerHTML = " ";
-                celdaResults.innerHTML = " ";
-                //Agregar el boton de editar a la celda acciones
-                tdAccionesLab.appendChild(btnEditar);
-
-                //Agregar el boton de eliminar a la celda acciones
-                tdAccionesLab.appendChild(btnEliminar);
-                if (result.isConfirmed) {
-                    Swal.fire(
-                        'Eliminado!',
-                        'La cita fue eliminado.',
-                        'success'
-                    )
-                }
-            })
-        });
+    let tbodyMed = document.querySelector("#tbl-receta tbody");
+    tbodyMed.innerHTML = "";
+    let fila = tbodyMed.insertRow();
+    let celdaDate = fila.insertCell();
+    let celdaPet = fila.insertCell();
+    let celdaDr = fila.insertCell();
+    let celdaMed = fila.insertCell();
+    let tdAccionesMed = fila.insertCell();
+    celdaDate.innerHTML = dFecha;
+    celdaPet.innerHTML = sPet;
+    celdaDr.innerHTML = sOwner;
+    celdaMed.innerHTML = sTest;
 
 
-    }
+    let btnEditar = document.createElement('button');
+    btnEditar.textContent = 'Editar';
+    btnEditar.type = 'button';
+    btnEditar.classList.add('btn-editar');
+    //Creacion del boton eliminar
+    let btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.type = 'button';
+    btnEliminar.classList.add('btn-eliminar');
 
+    //Agregar el boton de editar a la celda acciones
+    tdAccionesMed.appendChild(btnEditar);
+
+    //Agregar el boton de eliminar a la celda acciones
+    tdAccionesMed.appendChild(btnEliminar);
+
+    btnEliminar.addEventListener('click', () => {
+        Swal.fire({
+            title: 'Está seguro que desea eliminar el exámen?',
+            text: "La acción no se puede revertir!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar!'
+        }).then((result) => {
+            tbodyLab.innerHTML = "";
+            let celdaDate = fila.insertCell();
+            let celdaPet = fila.insertCell();
+            let celdaDr = fila.insertCell();
+            let celdaMed = fila.insertCell();
+            let tdAccionesMed = fila.insertCell();
+            celdaDate.innerHTML = " ";
+            celdaPet.innerHTML = " ";
+            celdaDr.innerHTML = " ";
+            celdaMed.innerHTML = " ";
+            //Agregar el boton de editar a la celda acciones
+            tdAccionesMed.appendChild(btnEditar);
+
+            //Agregar el boton de eliminar a la celda acciones
+            tdAccionesMed.appendChild(btnEliminar);
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Eliminado!',
+                    'El registro fue eliminado.',
+                    'success'
+                )
+            }
+        })
+    });
 
 
 }
+
+
 botonTx.addEventListener('click', llenarTablaTx);
 botonVac.addEventListener('click', llenarTablaVac);
 botonLab.addEventListener('click', llenarTablaLab);
