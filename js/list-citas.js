44839daae07bacb9b107c1d5be68267bdabe23a0
Sebastian Vindas <sebasvh2001@hotmail.com>
2022-07-28 16:55:14 -0600

    Cambios varios

---
+++ js/list-citas.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll("input");
+const modal = document.getElementById("modal-citas");
+const btnRol = document.getElementsByClassName("btn-ver");
+const span = document.getElementsByClassName("close")[0];
+
+let closeModal = () => {
+    modal.style.display = "none";
+}
+
+let displayModal = () => {
+    modal.style.display = "block";
+}
+
+for (const ver of btnRol) {
+    ver.addEventListener('click', displayModal);
+}
+
+span.addEventListener('click', closeModal);
+s
