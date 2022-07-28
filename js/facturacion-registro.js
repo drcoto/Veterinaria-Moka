59cbeaff2302355baa27571a097d248dd337529d
drcoto <106129878+drcoto@users.noreply.github.com>
2022-07-28 01:09:21 -0600

    update

--- js/facturacion-registro.js
+++ js/facturacion-registro.js
@@ -8,6 +8,9 @@ const inputPaciente = document.getElementById('txt-paciente');
 const inputSubtotal = document.getElementById('txt-subtotal');
 const inputDescuento = document.getElementById('txt-descuento');
 const inputTotal = document.getElementById('txt-total');
+const inputCantidad = document.getElementById('txt-cantidad');
+const inputProcedimiento = document.getElementById('txt-procedimientos');
+const inputPrecio = document.getElementById('txt-precio');
 const btnImprimir = document.getElementById('btn-imprimir');
 
 const validar = () => {
@@ -15,91 +18,115 @@ const validar = () => {
 
     if (inputJuridica.value == '') {
         error = true;
-        inputJuridica.classList.add('input-error');
+        inputJuridica.classList.add('input-invalid');
         inputJuridica.classList.remove('input-valid');
     } else {
-        inputJuridica.classList.remove('input-error');
+        inputJuridica.classList.remove('input-invalid');
         inputJuridica.classList.add('input-valid');
     }
 
     if (inputConsecutivo.value == '') {
         error = true;
-        inputConsecutivo.classList.add('input-error');
+        inputConsecutivo.classList.add('input-invalid');
         inputConsecutivo.classList.remove('input-valid');
     } else {
-        inputConsecutivo.classList.remove('input-error');
+        inputConsecutivo.classList.remove('input-invalid');
         inputConsecutivo.classList.add('input-valid');
     }
 
     if (inputFecha.value == '') {
         error = true;
-        inputFecha.classList.add('input-error');
+        inputFecha.classList.add('input-invalid');
         inputFecha.classList.remove('input-valid');
     } else {
-        inputFecha.classList.remove('input-error');
+        inputFecha.classList.remove('input-invalid');
         inputFecha.classList.add('input-valid');
     }
 
     if (inputCliente.value == '') {
         error = true;
-        inputCliente.classList.add('input-error');
+        inputCliente.classList.add('input-invalid');
         inputCliente.classList.remove('input-valid');
     } else {
-        inputCliente.classList.remove('input-error');
+        inputCliente.classList.remove('input-invalid');
         inputCliente.classList.add('input-valid');
     }
 
     if (inputEmail.value == '') {
         error = true;
-        inputEmail.classList.add('input-error');
+        inputEmail.classList.add('input-invalid');
         inputEmail.classList.remove('input-valid');
     } else {
-        inputEmail.classList.remove('input-error');
+        inputEmail.classList.remove('input-invalid');
         inputEmail.classList.add('input-valid');
     }
 
     if (inputTelefono.value == '') {
         error = true;
-        inputTelefono.classList.add('input-error');
+        inputTelefono.classList.add('input-invalid');
         inputTelefono.classList.remove('input-valid');
     } else {
-        inputTelefono.classList.remove('input-error');
+        inputTelefono.classList.remove('input-invalid');
         inputTelefono.classList.add('input-valid');
     }
 
     if (inputPaciente.value == '') {
         error = true;
-        inputPaciente.classList.add('input-error');
+        inputPaciente.classList.add('input-invalid');
         inputPaciente.classList.remove('input-valid');
     } else {
-        inputPaciente.classList.remove('input-error');
+        inputPaciente.classList.remove('input-invalid');
         inputPaciente.classList.add('input-valid');
+    }
+    if (inputCantidad.value == '') {
+        error = true;
+        inputCantidad.classList.add('input-invalid');
+        inputCantidad.classList.remove('input-valid');
+    } else {
+        inputCantidad.classList.remove('input-invalid');
+        inputCantidad.classList.add('input-valid');
+    }
+    if (inputProcedimiento.value == '') {
+        error = true;
+        inputProcedimiento.classList.add('input-invalid');
+        inputProcedimiento.classList.remove('input-valid');
+    } else {
+        inputProcedimiento.classList.remove('input-invalid');
+        inputProcedimiento.classList.add('input-valid');
+    }
+    if (inputPrecio.value == '') {
+        error = true;
+        inputPrecio.classList.add('input-invalid');
+        inputPrecio.classList.remove('input-valid');
+    } else {
+        inputPrecio.classList.remove('input-invalid');
+        inputPrecio.classList.add('input-valid');
     }
 
     if (inputSubtotal.value == '') {
         error = true;
-        inputSubtotal.classList.add('input-error');
+        inputSubtotal.classList.add('input-invalid');
         inputSubtotal.classList.remove('input-valid');
     } else {
-        inputSubtotal.classList.remove('input-error');
+        inputSubtotal.classList.remove('input-invalid');
         inputSubtotal.classList.add('input-valid');
     }
 
     if (inputDescuento.value == '') {
         error = true;
-        inputDescuento.classList.add('input-error');
+        inputDescuento.classList.add('input-invalid');
         inputDescuento.classList.remove('input-valid');
     } else {
-        inputDescuento.classList.remove('input-error');
+        inputDescuento.classList.remove('input-invalid');
         inputDescuento.classList.add('input-valid');
     }
 
     if (inputTotal.value == '') {
         error = true;
-        inputTotal.classList.add('input-error');
+        inputTotal.classList.add('input-invalid');
         inputTotal.classList.remove('input-valid');
     } else {
-        inputTotal.classList.remove('input-error');
+        inputTotal.classList.remove('input-invalid');
         inputTotal.classList.add('input-valid');
     }
     if (error == true) {
--- js/reserva-hotel.js
+++ js/reserva-hotel.js
@@ -3,62 +3,62 @@ const inputSalida = document.getElementById('fecha-salida');
 const inputAnimales = document.getElementById('cantidad-animales');
 const btnGuardar = document.getElementById('btn-registrar');
 
-const validar  = () => {
+const validarReserva = () => {
 
     let error = false;
 
-    if(inputEntrada.value == ''){
+    if (inputEntrada.value == '') {
         error = true;
-        inputEntrada.classList.add('input-error');
-    }else{
-        inputEntrada.classList.remove('input-error');
+        inputEntrada.classList.add('input-invalid');
+    } else {
+        inputEntrada.classList.remove('input-invalid');
     }
 
-    if(inputSalida.value == ''){
+    if (inputSalida.value == '') {
         error = true;
-        inputSalida.classList.add('input-error');
-    }else{
-        inputSalida.classList.remove('input-error');
+        inputSalida.classList.add('input-invalid');
+    } else {
+        inputSalida.classList.remove('input-invalid');
     }
 
-    if(inputAnimales.value == ''){
+    if (inputAnimales.value == '') {
         error = true;
-        inputAnimales.classList.add('input-error');
-    }else{
-        inputAnimales.classList.remove('input-error')
+        inputAnimales.classList.add('input-invalid');
+    } else {
+        inputAnimales.classList.remove('input-invalid')
     }
-    
-    if(error == true){
-       Swal.fire({
-        'icon' : 'warning',
-        'title' : 'Datos ingresados incorrectamente',
-        'text': 'Favor revise los campos resaltados',
-        'confirmButtonText' : 'Entendido'
-       });
 
-    }else{
-        obtenerDatos();
+    if (error == true) {
+        Swal.fire({
+            'icon': 'warning',
+            'title': 'Datos ingresados incorrectamente',
+            'text': 'Favor revise los campos resaltados',
+            'confirmButtonText': 'Entendido'
+        });
+
+    } else {
+        obtenerDatosRegistro();
     }
-    
-}; 
-const obtenerDatos = () => {
+
+};
+const obtenerDatosRegistro = () => {
     //variable tipo JSON
     let reserva = {
-        'date' : inputEntrada.value,
-        'cant-cuartos' : inputSalida.value,
-        'cant-animales' : inputAnimales.value
+        'date': inputEntrada.value,
+        'cant-cuartos': inputSalida.value,
+        'cant-animales': inputAnimales.value
     };
     console.log(reserva);
     Swal.fire({
         'icon': 'success',
-        'title' : 'Reserva realizada exitosamente',
-        'text' : 'Su numero de confirmacion le llegara al correo',
-        'confirmButtonText' : 'Continuar'
-    }).then(()=>{
+        'title': 'Reserva realizada exitosamente',
+        'text': 'Su numero de confirmacion le llegara al correo',
+        'confirmButtonText': 'Continuar'
+    }).then(() => {
         window.location.href = 'reserva-listado.html'
 
     });
-   
+
 };
 
-btnGuardar.addEventListener('click', validar)
+btnGuardar.addEventListener('click', validarReserva);
